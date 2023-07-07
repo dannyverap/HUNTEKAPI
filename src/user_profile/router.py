e9d74a34aa385b38b992c3b2509a5f8b83f0bc14
dannyverap <danny.verap@gmail.com>
2023-07-07 17:01:41 -0500

    schema

--- src/user_profile/router.py
+++ src/user_profile/router.py
@@ -4,14 +4,14 @@ from fastapi import status, HTTPException, APIRouter, Security
 from fastapi.responses import JSONResponse
 
 
-user_profile_router = APIRouter()
+# user_profile_router = APIRouter()
 
-@user_profile_router.get("/user_profile")
-async def get_user_profile(
-    db: Session = Depends(get_db),
-    skip: int = 0,
-    limit: int = 100,
-    
-)
-    :
-    return JSONResponse(content={"success":True,"msg":"Base module installed!"})
+# @user_profile_router.get("/user_profile")
+# async def get_user_profile(
+#     db: Session = Depends(get_db),
+#     skip: int = 0,
+#     limit: int = 100,
+#     ##! current user?
+# )
+#     :
+#     return JSONResponse(content={"success":True,"msg":"Base module installed!"})
